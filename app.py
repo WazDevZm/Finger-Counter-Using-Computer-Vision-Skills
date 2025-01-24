7e5521c0ce305a566ee0f16eacd2ca887c2ec317
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-24 12:56:29 +0200

    Added more code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import cv2
 import time
 import HandTrackingModule as htm
+import os
 
 
 wCam, hCam = 640, 480
@@ -9,3 +10,17 @@ wCam, hCam = 640, 480
 cap = cv2.VideoCapture(0)
 cap.set (3, wCam)
 cap.set (4, hCam)
+
+
+folderPath = "Images"
+myList = os.listdir(folderPath )
+print(myList)
+
+
+#importing our images fromm the files to the project
+
+while True:
+    
+    success, img = cap.read()
+    cv2.imshow("Computer Vision", img)
+    cv2.waitKey(1)
